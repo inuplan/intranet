a17b7f0eeeb05ce5c83c1709c71e7597af77bb69
Johnny <jms@inuplan.gl>
2015-11-26 12:36:45 -0300

    Added Inuplan.Tests and removed previous project
    also added a simple script file which can run the test from the console.

---
+++ Inuplan.Tests/WebAPI/TestManagementPostController.cs
@@ -0,0 +1,17 @@
+using System;
+using NUnit;
+using NUnit.Framework;
+
+namespace Inuplan.Tests
+{
+	[TestFixture]
+	public class TestManagementPostController
+	{
+		[Test]
+		public void Test()
+		{
+			Assert.IsTrue (true);
+		}
+	}
+}
+
