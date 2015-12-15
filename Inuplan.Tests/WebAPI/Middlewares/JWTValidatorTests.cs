e20d5926b37c133d950c7614d5a764910c6ae14a
Johnny <jms@inuplan.gl>
2015-12-15 17:13:01 -0300

    Changed the Tests project to use xUnit

---
+++ Inuplan.Tests/WebAPI/Middlewares/JWTValidatorTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Inuplan.Tests.WebAPI.Middlewares
+{
+    public class JWTValidatorTests
+    {
+        [Fact]
+        public void Test()
+        {
+            Console.WriteLine("Test passed!");
+        }
+    }
+}
