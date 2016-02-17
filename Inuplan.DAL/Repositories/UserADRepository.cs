b332fa9be835720e4c532ed4577db4237b2eb3c7
Johnny <jms@inuplan.gl>
2016-02-17 11:18:32 -0300

    Finished writing repo for users_ad
    Still needs to perhaps write some tests.
    Fill out some application settings values.

--- Inuplan.DAL/Repositories/UserADRepository.cs
+++ Inuplan.DAL/Repositories/UserADRepository.cs
@@ -24,66 +24,174 @@ namespace Inuplan.DAL.Repositories
     using Common.Repositories;
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using System.Threading.Tasks;
     using Optional;
     using System.DirectoryServices.AccountManagement;
+
+    /// <summary>
+    /// Retrieves user information from Active Directory
+    /// Only GET method is supported.
+    /// </summary>
     public class UserADRepository : IRepository<string, User>
     {
+        /// <summary>
+        /// The principal context from which every query is run against
+        /// </summary>
         private readonly PrincipalContext ctx;
+        
+        /// <summary>
+        /// Lock mechanism for multithreaded access
+        /// </summary>
+        private readonly object locking;
 
+        /// <summary>
+        /// Dispose pattern implementation.
+        /// Indicates whether dispose has been called multiple times.
+        /// </summary>
+        private bool disposed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserADRepository"/> class.
+        /// </summary>
+        /// <param name="ctx">The principal context from which every query is run against</param>
         public UserADRepository(PrincipalContext ctx)
         {
             this.ctx = ctx;
+            locking = new object();
         }
 
+        /// <summary>
+        /// Not supported operation
+        /// </summary>
+        /// <param name="entity">N/A</param>
+        /// <exception cref="NotSupportedException">Not supported operation</exception>
+        /// <returns>N/A</returns>
         public Task<Option<User>> Create(User entity)
         {
-            throw new NotSupportedException();
+            throw new NotSupportedException("Not supported operation!");
         }
 
+        /// <summary>
+        /// Not supported operation
+        /// </summary>
+        /// <param name="entity">N/A</param>
+        /// <exception cref="NotSupportedException">Not supported operation</exception>
+        /// <returns>N/A</returns>
         public Task<Option<bool>> Delete(User entity)
         {
-            throw new NotSupportedException();
+            throw new NotSupportedException("Not supported operation!");
         }
 
+        /// <summary>
+        /// Not supported operation
+        /// </summary>
+        /// <param name="key">N/A</param>
+        /// <exception cref="NotSupportedException">Not supported operation</exception>
+        /// <returns>N/A</returns>
         public Task<Option<bool>> Delete(string key)
         {
-            throw new NotSupportedException();
+            throw new NotSupportedException("Not supported operation!");
         }
 
+        /// <summary>
+        /// Returns a user with the given username.
+        /// Or none, if none found.
+        /// </summary>
+        /// <param name="key">The username</param>
+        /// <returns>An awaitable optional user</returns>
         public Task<Option<User>> Get(string key)
         {
-            // var adUser = UserPrincipal.FindByIdentity(principalContext, c.Username);
-            // TODO: Only this method can work...
-            var adUser = UserPrincipal.FindByIdentity(ctx, key);
-            throw new NotImplementedException();
+            var adUser = UserPrincipal.FindByIdentity(ctx, key).SomeNotNull();
+
+            var user = adUser.Map(u =>
+            {
+                return new User
+                {
+                    Email = u.EmailAddress,
+                    FirstName = u.GivenName,
+                    LastName = u.Surname,
+                    Username = u.SamAccountName
+                };
+            });
+
+            return Task.FromResult(user);
         }
 
+        /// <summary>
+        /// Not supported operation
+        /// </summary>
+        /// <param name="skip">N/A</param>
+        /// <param name="take">N/A</param>
+        /// <exception cref="NotSupportedException">Not supported operation</exception>
+        /// <returns>N/A</returns>
         public Task<List<User>> Get(int skip, int take)
         {
-            throw new NotSupportedException();
+            throw new NotSupportedException("Not supported operation!");
         }
 
+        /// <summary>
+        /// Not supported operation
+        /// </summary>
+        /// <exception cref="NotSupportedException">Not supported operation</exception>
+        /// <returns>N/A</returns>
         public Task<List<User>> GetAll()
         {
-            throw new NotSupportedException();
+            throw new NotSupportedException("Not supported operation!");
         }
 
+        /// <summary>
+        /// Not supported operation
+        /// </summary>
+        /// <param name="id">N/A</param>
+        /// <exception cref="NotSupportedException">Not supported operation</exception>
+        /// <returns>N/A</returns>
         public Task<Option<User>> GetByID(int id)
         {
-            throw new NotSupportedException();
+            throw new NotSupportedException("Not supported operation!");
         }
 
+        /// <summary>
+        /// Not supported operation
+        /// </summary>
+        /// <param name="key">N/A</param>
+        /// <param name="entity">N/A</param>
+        /// <exception cref="NotSupportedException">Not supported operation</exception>
+        /// <returns>N/A</returns>
         public Task<Option<bool>> Update(string key, User entity)
         {
-            throw new NotSupportedException();
+            throw new NotSupportedException("Not supported operation!");
         }
 
+        /// <summary>
+        /// A protected dispose method for the dispose pattern.
+        /// </summary>
+        /// <param name="disposing">Indicates whether to dispose this resource</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            lock(locking)
+            {
+                if(disposed)
+                {
+                    return;
+                }
+
+                if(disposing)
+                {
+                    if(ctx != null)
+                    {
+                        ctx.Dispose();
+                        disposed = true;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Implementation of the dispose pattern.
+        /// </summary>
         public void Dispose()
         {
-            // No resources to dispose
+            Dispose(true);
         }
     }
 }
--- Inuplan.WebAPI/App_Start/DependencyConfig.cs
+++ Inuplan.WebAPI/App_Start/DependencyConfig.cs
@@ -17,6 +17,9 @@
 namespace Inuplan.WebAPI.App_Start
 {
     using System.Configuration;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.DirectoryServices.AccountManagement;
     using System.Security.Cryptography;
     using System.Web.Http;
     using Autofac;
@@ -31,12 +34,15 @@ namespace Inuplan.WebAPI.App_Start
     using Inuplan.WebAPI.Middlewares.JWT;
     using Jose;
     using Owin;
-    using System.Data.SqlClient;
-    using System.Data;    /// <summary>
-                          /// Setup the configuration for the Inversion of Control container
-                          /// </summary>
+
+    /// <summary>
+    /// Setup the configuration for the Inversion of Control container
+    /// </summary>
     public static class DependencyConfig
     {
+        /// <summary>
+        /// The autofac IoC container
+        /// </summary>
         private static IContainer container;
 
         /// <summary>
@@ -47,63 +53,57 @@ namespace Inuplan.WebAPI.App_Start
         public static void RegisterContainer(HttpConfiguration config, IAppBuilder app)
         {
             var builder = new ContainerBuilder();
-            var sha256 = SHA256.Create();
-            
-            // this ensures that the string -> byte[] has size of 256 bits.
-            // which is a requirement in the HS* crypto algorithm
-            var secret = sha256.ComputeHash(Helpers.GetBytes(ConfigurationManager.AppSettings["Secret"]));
 
-            // Register your Web API controllers.
+            // Register Web API controllers
             builder.Register(ctx => new ManagementPostController(ctx.ResolveKeyed<IRepository<int, Post>>(ServiceKeys.ManagementPosts)));
 
-            // Register the Autofac filter provider.
+            // Autofac filter provider
             builder.RegisterWebApiFilterProvider(config);
 
-            // Register types here...
+            // AD repository
+            builder.Register(ctx => ConfigurationManager.AppSettings["domainName"]).Keyed<string>(ServiceKeys.DomainName);
+            builder.Register(ctx => new PrincipalContext(ContextType.Domain, ctx.ResolveKeyed<string>(ServiceKeys.DomainName)));
+            builder.RegisterType<UserADRepository>().Keyed<IRepository<string, User>>(ServiceKeys.UserActiveDirectory);
+
+            // SQL repositories
             builder.Register(ctx => new SqlConnection(ConfigurationManager.AppSettings["connectionString"])).As<IDbConnection>();
-            builder.RegisterType<ManagementPostRepository>().Keyed<IRepository<int, Post>>(ServiceKeys.ManagementPosts);
             builder.RegisterType<UserDatabaseRepository>().Keyed<IRepository<string, User>>(ServiceKeys.UserDatabase);
+            builder.RegisterType<ManagementPostRepository>().Keyed<IRepository<int, Post>>(ServiceKeys.ManagementPosts);
+            builder.RegisterType<GeneralPostRepository>().Keyed<IRepository<int, Post>>(ServiceKeys.GeneralPosts);
+
+            // Middleware JWT key
+            var sha256 = SHA256.Create();
+            var secret = sha256.ComputeHash(Helpers.GetBytes(ConfigurationManager.AppSettings["secret"]));
+
+            // Middleware JWT options
+            builder.Register(ctx => new NewtonsoftMapper()).As<IJsonMapper>();
             builder.Register(ctx => new JWTValidatorOptions
             {
-                LogInvalidSignature = (expected, actual) =>
-                {
-                    /* Insert logger here... */
-                },
-
-                LogExpired = expiration =>
-                {
-                    /* Insert logger here.. */
-                },
-
-                LogError = (msg, obj) =>
-                {
-                    /* Insert logger here... */
-                },
-
                 Mapper = ctx.Resolve<IJsonMapper>(),
-
                 Secret = secret
             });
             builder.Register(ctx => new JWTClaimsRetrieverOptions
             {
                 UserDatabaseRepository = ctx.ResolveKeyed<IRepository<string, User>>(ServiceKeys.UserDatabase)
             });
-            builder.Register(ctx => new NewtonsoftMapper()).As<IJsonMapper>();
 
             // Build container
             container = builder.Build();
 
-            // Register Autofac Middleware
+            // Autofac Middleware
             app.UseAutofacMiddleware(container);
 
             // Set the dependency resolver
             config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
         }
 
+        /// <summary>
+        /// Exposes the dependency injection container
+        /// </summary>
+        /// <returns>An autofac IoC container</returns>
         public static IContainer Container()
         {
             return container;
         }
-
     }
 }
