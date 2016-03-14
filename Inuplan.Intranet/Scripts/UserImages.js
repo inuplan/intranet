75451af5ad112f0a8d9ed18f9785094eb99447bd
Johnny <jms@inuplan.gl>
2016-03-14 16:48:24 -0300

    @WIP Somewhat functioning view, with ajax.
    TODO: Maybe make a modal popup, when image is clicked? Which also displays the appropriate comments.

--- Inuplan.Intranet/Scripts/UserImages.js
+++ Inuplan.Intranet/Scripts/UserImages.js
@@ -1,11 +1,27 @@
-﻿function UserImagesViewModel(username) {
+﻿function UserImagesViewModel() {
     var self = this;
-    var url = "/api/" + username + "/image";
 
     // Data
     self.images = ko.observableArray([]);
+    var thumbnail = ko.observable();
 
-    $.getJSON(url, function (allData) {
-        console.log(allData);
+    // Functions
+    $.getJSON(url_path, function (allData) {
+        var image = $.map(allData, function (item) {
+            return new ImageViewModel(item);
+        });
+        self.images(image);
     });
+}
+
+function ImageViewModel(data) {
+    var self = this;
+    this.thumbnailUrl = ko.observable(data.PathThumbnailUrl);
+    this.originalUrl = ko.observable(data.PathOriginalUrl);
+    if (!$.isEmptyObject(data.Comments)) {
+        this.comments = data.Comments.length;
+    }
+    else {
+        this.comments = 0;
+    }
 }
