387214bc586d79beb4f64456cefbf358cc4ba28c
Johnny <jms@inuplan.gl>
2016-03-14 14:46:30 -0300

    WIP can now correctly run javascript

--- Inuplan.Intranet/Scripts/UserImages.js
+++ Inuplan.Intranet/Scripts/UserImages.js
@@ -1,11 +1,11 @@
 ﻿function UserImagesViewModel(username) {
     var self = this;
-    var url = "/api/" + username + "image";
+    var url = "/api/" + username + "/image";
 
     // Data
     self.images = ko.observableArray([]);
 
     $.getJSON(url, function (allData) {
-
+        console.log(allData);
     });
 }
