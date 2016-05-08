ad32288d96a177b2a669dc1184f8a7e327ab491c
Johnny <jms@inuplan.gl>
2016-05-08 01:38:27 -0200

    feat: Add references to react and babel-cli

--- Inuplan.Intranet.vNext/Startup.cs
+++ Inuplan.Intranet.vNext/Startup.cs
@@ -25,7 +25,6 @@ namespace Inuplan.Intranet.vNext
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
-    using React.AspNet;
 
     public class Startup
     {
@@ -43,8 +42,6 @@ namespace Inuplan.Intranet.vNext
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddReact();
-
             // Add framework services.
             services.AddMvc();
         }
@@ -66,26 +63,6 @@ namespace Inuplan.Intranet.vNext
             }
 
             app.UseIISPlatformHandler();
-
-            // Initialise ReactJS.NET. Must be before static files.
-            app.UseReact(config =>
-            {
-                // If you want to use server-side rendering of React components,
-                // add all the necessary JavaScript files here. This includes
-                // your components as well as all of their dependencies.
-                // See http://reactjs.net/ for more information. Example:
-                //config
-                //    .AddScript("~/Scripts/First.jsx")
-                //    .AddScript("~/Scripts/Second.jsx");
-
-                // If you use an external build too (for example, Babel, Webpack,
-                // Browserify or Gulp), you can improve performance by disabling
-                // ReactJS.NET's version of Babel and loading the pre-transpiled
-                // scripts. Example:
-                //config
-                //    .SetLoadBabel(false)
-                //    .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
-            });
 
             app.UseStaticFiles();
 
