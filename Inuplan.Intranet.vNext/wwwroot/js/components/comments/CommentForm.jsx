bffa24dc96522046ae09016dd3be1becdbe121a5
Johnny <jms@inuplan.gl>
2016-05-23 12:48:17 -0200

    feat: Remove 2 obsolete web site projects
    Using the new asp.net mvc core framework RC 2.0.
    This is possible because the architecture is independent of the API.

--- Inuplan.Intranet.vNext/wwwroot/js/components/comments/CommentForm.jsx
+++
@@ -1,9 +0,0 @@
-﻿var CommentForm = React.createClass({
-  render: function() {
-    return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
-    );
-  }
-});
--- Inuplan.Intranet/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Inuplan.Intranet.MvcApplication" Language="C#" %>
