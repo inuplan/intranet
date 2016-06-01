42740b44f08468fd6bd42075711d0de42863ecca
Johnny <jms@inuplan.gl>
2016-05-31 22:54:24 -0200

    feat: Use a hand pointer when hovering over image

--- Inuplan.Intranet.Core/wwwroot/js/components/images/Image.jsx
+++ Inuplan.Intranet.Core/wwwroot/js/components/images/Image.jsx
@@ -5,7 +5,7 @@
     render: function () {
         return (
         <div>
-            <a onClick={this.toggleModal}>
+            <a href="#" onClick={this.toggleModal}>
                 <img src={this.props.data.PreviewUrl} className="img-thumbnail" alt={this.props.data.Filename} />
             </a>
         </div>
