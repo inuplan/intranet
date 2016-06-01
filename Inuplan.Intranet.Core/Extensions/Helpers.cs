3f1d5cc3559ced5967ff28efe34f76e4a0800c11
Johnny <jms@inuplan.gl>
2016-06-01 12:56:17 -0200

    wip: Separate the menu into a ViewComponent

--- Inuplan.Intranet.Core/Extensions/Helpers.cs
+++ Inuplan.Intranet.Core/Extensions/Helpers.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Security.Claims;
+using System.Security.Principal;
 
 namespace Inuplan.Intranet.Core.Extensions
 {
     public static class Helpers
     {
-        public static string UsernameWithoutDomain(this ClaimsPrincipal principal)
+        public static string UsernameWithoutDomain(this IIdentity identity)
         {
-            var username = principal.Identity.Name;
+            var username = identity.Name;
             return username.Substring(username.LastIndexOf('\\') + 1);
         }
     }
