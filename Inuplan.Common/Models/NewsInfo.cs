d07f7628557692c93950a842e5ac3c03139d53d4
Johnny <jms@inuplan.gl>
2016-07-10 02:12:35 -0200

    feat: Add a NewsInfo model
    This will model a single "What's new?" item.
    The idea being, that a running list of info will be displayed on the
    intranet page. Details still unclear.

---
+++ Inuplan.Common/Models/NewsInfo.cs
@@ -0,0 +1,50 @@
+﻿// Copyright © 2015 Inuplan
+// 
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+// OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+namespace Inuplan.Common.Models
+{
+    using System;
+
+    /// <summary>
+    /// Represents a single news info in a "What's new?" module
+    /// </summary>
+    public class NewsInfo
+    {
+        /// <summary>
+        /// Gets or sets the ID of the <see cref="NewsInfo"/> item
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets when the info happened
+        /// </summary>
+        public DateTime On { get; set; }
+
+        /// <summary>
+        /// Gets or sets the info description of the news
+        /// </summary>
+        public string Info { get; set; }
+
+        /// <summary>
+        /// Gets or sets the subject for this news info item
+        /// </summary>
+        public User Subject { get; set; }
+    }
+}
