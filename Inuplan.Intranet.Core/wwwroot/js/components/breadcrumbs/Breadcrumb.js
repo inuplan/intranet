3a0fe9bf1d035d69a5af858796bf03c72cca50f0
Johnny <jms@inuplan.gl>
2016-09-06 21:45:19 -0200

    feat: Breadcrumb component

---
+++ Inuplan.Intranet.Core/wwwroot/js/components/breadcrumbs/Breadcrumb.js
@@ -0,0 +1,26 @@
+﻿import React from 'react'
+import { Link } from 'react-router'
+
+export class Breadcrumb extends React.Component {
+    render() {
+        return  <ol className="breadcrumb">
+                    {this.props.children}
+                </ol>
+    }
+}
+
+Breadcrumb.Item = class Item extends React.Component {
+    render() {
+        const { href, active } = this.props;
+        if(active) return   <li className="active">
+                                {this.props.children}
+                            </li>
+
+        return  <li>
+                    <Link to={href}>
+                        {this.props.children}
+                    </Link>
+                </li>
+
+    }
+}
