ab6b5faa993c4ccfce8455008cef5e441e82419a
Johnny <jms@inuplan.gl>
2016-10-31 11:54:21 -0300

    feat: Suppress warning when property and variable naming is the same, which could be writting as single word
    Reason for supression is that we want to keep the variable name and the property name separate even though
    they currently have the same name. Because if we want to change the variable name in the future we will also change
    the property name accidentally (tight coupled).
    
    TL;DR: We want to be able to refactor without complications.

---
+++ Inuplan.WebAPI/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Code", "RECS0069:Redundant explicit property name", Justification = "To decouple the name from the variable.", Scope = "member", Target = "~M:Inuplan.WebAPI.Controllers.DefaultController.ImageUrl(Inuplan.Common.Models.Image,System.String)~System.String")]
+
