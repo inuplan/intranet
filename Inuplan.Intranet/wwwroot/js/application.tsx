30b0d39b8a72a686ec6eb4f0a4ae0e3611137ced
Johnny <jms@inuplan.gl>
2016-12-07 20:02:12 -0300

    feat: Remove typescript (not on this branch)

--- Inuplan.Intranet/wwwroot/js/application.tsx
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../typings/index.d.ts" />
-
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { connect, Provider } from 'react-redux'
-
-ReactDOM.render(
-    <Provider store={ {} } />
-    , document.getElementById("content"));
