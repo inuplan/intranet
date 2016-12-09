69c2ae96690ba727737c2c5450563bddca87a8b0
Johnny <jms@inuplan.gl>
2016-12-09 14:46:02 -0300

    feat: Add transpiled jsx to ignore

--- Inuplan.Intranet/wwwroot/ts/dist/app.jsx
+++
@@ -1,9 +0,0 @@
-/// <reference path="./typings/index.d.ts" />
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Router, browserHistory } from 'react-router';
-import { Provider } from 'react-redux';
-ReactDOM.render(<Provider>
-        <Router history={browserHistory}>
-        </Router>
-    </Provider>, document.getElementById("content"));
