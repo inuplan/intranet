c9df16d72f8d4eb3e55682b75280bc1ab71762a5
Johnny <jms@inuplan.gl>
2016-12-18 01:08:14 -0300

    feat: Define dispatch action type for response

--- Inuplan.Intranet/wwwroot/ts/src/utilities/utils.ts
+++ Inuplan.Intranet/wwwroot/ts/src/utilities/utils.ts
@@ -32,19 +32,19 @@ export const responseHandler =  <T>(dispatch:Dispatch<Root>) => (onSuccess: (r:I
     else {
         switch(response.status) {
             case 400:
-                dispatch(setError({ title: "400 Bad Request", message: "The request was not well-formed"}));
+                dispatch<any>(setError({ title: "400 Bad Request", message: "The request was not well-formed"}));
                 break;
             case 404:
-                dispatch(setError({ title: "404 Not Found", message: "Could not find resource"}));
+                dispatch<any>(setError({ title: "404 Not Found", message: "Could not find resource"}));
                 break;
             case 408:
-                dispatch(setError({ title: "408 Request Timeout", message: "The server did not respond in time"}));
+                dispatch<any>(setError({ title: "408 Request Timeout", message: "The server did not respond in time"}));
                 break;
             case 500:
-                dispatch(setError({ title: "500 Server Error", message: "Something went wrong with the API-server"}));
+                dispatch<any>(setError({ title: "500 Server Error", message: "Something went wrong with the API-server"}));
                 break;
             default:
-                dispatch(setError({ title: "Oops", message: "Something went wrong!"}));
+                dispatch<any>(setError({ title: "Oops", message: "Something went wrong!"}));
                 break;
         }
     }
