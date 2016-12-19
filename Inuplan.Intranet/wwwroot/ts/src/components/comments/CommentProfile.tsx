0fcf1e42b0a97eed87427e04b74f75f88ba839fd
Johnny <jms@inuplan.gl>
2016-12-19 04:08:04 -0300

    style: CommentProfile format document

--- Inuplan.Intranet/wwwroot/ts/src/components/comments/CommentProfile.tsx
+++ Inuplan.Intranet/wwwroot/ts/src/components/comments/CommentProfile.tsx
@@ -4,11 +4,9 @@ import { Image, Media } from 'react-bootstrap'
 export class CommentProfile extends React.Component<any, any> {
     render() {
         return  <Media.Left className="comment-profile">
-                    <Image
-                        src="/images/person_icon.svg"
-                        style={{ width: "64px", height: "64px" }}
-                        className="media-object"
-                    />
+                    <Image src="/images/person_icon.svg"
+                           style={{ width: "64px", height: "64px" }}
+                           className="media-object"/>
                     {this.props.children}
                 </Media.Left>
     }
