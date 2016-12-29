d3fa51b4776081cf908aeb4f331e0003b8db2a15
Johnny <jms@inuplan.gl>
2016-12-29 01:37:03 -0300

    feat: Add rollup configuration files

--- Inuplan.Intranet/wwwroot/ts/rollup.config.release.js
+++ Inuplan.Intranet/wwwroot/ts/rollup.config.release.js
@@ -1,11 +1,12 @@
 import buble from "rollup-plugin-buble";
+import uglify from "rollup-plugin-uglify";
 
 export default {
   entry: "dist/app.js",
-  dest: "dist/bundle.js",
+  dest: "dist/bundle.min.js",
   format: "cjs",
   plugins: [
-    buble()
-  ],
-  sourceMap: "inline",
+    buble(),
+    uglify()
+  ]
 };
