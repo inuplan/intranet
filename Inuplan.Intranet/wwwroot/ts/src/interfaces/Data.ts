f355eb644df2dea36724f12e4c29a1b72cc5e30e
Johnny <jms@inuplan.gl>
2016-12-29 00:14:25 -0300

    style: Tslint warnings

--- Inuplan.Intranet/wwwroot/ts/src/interfaces/Data.ts
+++ Inuplan.Intranet/wwwroot/ts/src/interfaces/Data.ts
@@ -8,184 +8,184 @@ export const enum WhatsNewType {
 export declare namespace Data {
     // UserDTO
     export interface User {
-        ID: number
-        DisplayName: string
-        Username: string
-        FirstName: string
-        LastName: string
-        Email: string
-        ProfileImage: string
-        IsAdmin: boolean
-        Role: Role[]
+        ID: number;
+        DisplayName: string;
+        Username: string;
+        FirstName: string;
+        LastName: string;
+        Email: string;
+        ProfileImage: string;
+        IsAdmin: boolean;
+        Role: Role[];
     }
 
     export interface Role {
-        ID: number
-        Name: string
+        ID: number;
+        Name: string;
     }
-    
+
     // state: images type
     export interface Image {
-        ImageID: number
-        Filename: string
-        Extension: string
-        OriginalUrl: string
-        PreviewUrl: string
-        ThumbnailUrl: string
-        CommentCount: number
-        Uploaded: Date
+        ImageID: number;
+        Filename: string;
+        Extension: string;
+        OriginalUrl: string;
+        PreviewUrl: string;
+        ThumbnailUrl: string;
+        CommentCount: number;
+        Uploaded: Date;
     }
 
     // state: comment type
     export interface Comment {
-        CommentID: number
-        AuthorID: number
-        Deleted: boolean
-        PostedOn: Date
-        Text: string
-        Replies: Comment[]
-        Edited: boolean
+        CommentID: number;
+        AuthorID: number;
+        Deleted: boolean;
+        PostedOn: Date;
+        Text: string;
+        Replies: Comment[];
+        Edited: boolean;
     }
 
     // state: forumtitle type
     export interface ForumTitle {
-        ID: number
-        IsPublished: boolean
-        Sticky: boolean
-        CreatedOn: Date
-        AuthorID: number
-        Deleted: boolean
-        IsModified: boolean
-        Title: string
-        LastModified: Date
-        LatestComment: Comment
-        CommentCount: number
-        ViewedBy: number[]
+        ID: number;
+        IsPublished: boolean;
+        Sticky: boolean;
+        CreatedOn: Date;
+        AuthorID: number;
+        Deleted: boolean;
+        IsModified: boolean;
+        Title: string;
+        LastModified: Date;
+        LatestComment: Comment;
+        CommentCount: number;
+        ViewedBy: number[];
     }
 
     // state: whatsnew type
-    export interface WhatsNew{
-        ID: number
-        Type: WhatsNewType
-        Item: WhatsNewComment | WhatsNewImage | WhatsNewForumPost
-        On: Date
-        AuthorID: number
+    export interface WhatsNew {
+        ID: number;
+        Type: WhatsNewType;
+        Item: WhatsNewComment | WhatsNewImage | WhatsNewForumPost;
+        On: Date;
+        AuthorID: number;
     }
 
     interface WhatsNewImage {
-        ImageID: number
-        Extension: string
-        Filename: string
-        OriginalUrl: string
-        PreviewUrl: string
-        ThumbnailUrl: string
-        Uploaded: Date
+        ImageID: number;
+        Extension: string;
+        Filename: string;
+        OriginalUrl: string;
+        PreviewUrl: string;
+        ThumbnailUrl: string;
+        Uploaded: Date;
     }
 
     interface WhatsNewComment {
-        ID: number
-        Text: string
-        ImageID: number
-        ImageUploadedBy: User
-        Filename: string
+        ID: number;
+        Text: string;
+        ImageID: number;
+        ImageUploadedBy: User;
+        Filename: string;
     }
 
     interface WhatsNewForumPost {
-        ID: number
-        Title: string
-        Text: string
-        Sticky: boolean
-        CommentCount: number
+        ID: number;
+        Title: string;
+        Text: string;
+        Sticky: boolean;
+        CommentCount: number;
     }
 
     export namespace Raw {
         export interface Pagination<T> {
-            CurrentPage: number
-            CurrentItems: T[]
-            TotalPages: number
+            CurrentPage: number;
+            CurrentItems: T[];
+            TotalPages: number;
         }
 
         export interface WhatsNewItem {
-            ID: number
-            On: Date
-            Type: WhatsNewType
-            Item: ImageDTO | WhatsNewImageCommentDTO | ThreadPostContentDTO
+            ID: number;
+            On: Date;
+            Type: WhatsNewType;
+            Item: ImageDTO | WhatsNewImageCommentDTO | ThreadPostContentDTO;
         }
 
         export interface WhatsNewImageCommentDTO {
-            ID: number
-            ImageID: number
-            Deleted: boolean
-            Author: User
-            Text: string
-            ImageUploadedBy: User
-            Filename: string
+            ID: number;
+            ImageID: number;
+            Deleted: boolean;
+            Author: User;
+            Text: string;
+            ImageUploadedBy: User;
+            Filename: string;
         }
 
         export interface ImageDTO {
-            Author: User
-            ImageID: number
-            Uploaded: Date
-            Filename: string
-            Extension: string
-            OriginalUrl: string
-            PreviewUrl: string
-            ThumbnailUrl: string
-            CommentCount: number
+            Author: User;
+            ImageID: number;
+            Uploaded: Date;
+            Filename: string;
+            Extension: string;
+            OriginalUrl: string;
+            PreviewUrl: string;
+            ThumbnailUrl: string;
+            CommentCount: number;
         }
 
         export interface ThreadPostContentDTO {
-            Header: ThreadPostTitleDTO
-            ThreadID: number
-            Text: string
+            Header: ThreadPostTitleDTO;
+            ThreadID: number;
+            Text: string;
         }
 
         export interface ThreadPostTitleDTO {
-            ID: number
-            IsPublished: boolean
-            Sticky: boolean
-            CreatedOn: Date
-            Author: User
-            Deleted: boolean
-            IsModified: boolean
-            Title: string
-            LatestComment: Comment
-            LastModified: Date
-            CommentCount: number
-            ViewedBy: User[]
+            ID: number;
+            IsPublished: boolean;
+            Sticky: boolean;
+            CreatedOn: Date;
+            Author: User;
+            Deleted: boolean;
+            IsModified: boolean;
+            Title: string;
+            LatestComment: Comment;
+            LastModified: Date;
+            CommentCount: number;
+            ViewedBy: User[];
         }
 
         export interface Comment {
-            ParentID: number
-            ID: number
-            ContextID: number
-            Deleted: boolean
-            PostedOn: Date
-            Author: User
-            Text: string
-            Edited: boolean
-            Replies?: Comment[]
+            ParentID: number;
+            ID: number;
+            ContextID: number;
+            Deleted: boolean;
+            PostedOn: Date;
+            Author: User;
+            Text: string;
+            Edited: boolean;
+            Replies?: Comment[];
         }
 
         namespace Models {
             export interface ThreadPostTitle {
-                ThreadID: number
-                IsPublished: boolean
-                Sticky: boolean
-                CreatedOn: Date
-                Author: User
-                Deleted: boolean
-                IsModified: boolean
-                Title: string
-                LastModified: Date
-                LatestComment: number
-                ViewedBy: Data.User[]
+                ThreadID: number;
+                IsPublished: boolean;
+                Sticky: boolean;
+                CreatedOn: Date;
+                Author: User;
+                Deleted: boolean;
+                IsModified: boolean;
+                Title: string;
+                LastModified: Date;
+                LatestComment: number;
+                ViewedBy: Data.User[];
             }
 
             export interface ThreadPostContent {
-                Header: Partial<ThreadPostTitle>
-                ThreadID: number
-                Text: string
+                Header: Partial<ThreadPostTitle>;
+                ThreadID: number;
+                Text: string;
             }
         }
     }
